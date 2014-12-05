42768c89a23d1a1844ab468d4c10f07013319dc8
leogiese <leogiese0@gmail.com>
2014-12-06 07:59:51 +0800

    deleted

--- dist/lib/extendActor.js
+++
@@ -1,6 +0,0 @@
-System.register("../../lib/extendActor", [], function() {
-  "use strict";
-  var __moduleName = "../../lib/extendActor";
-  return {};
-});
-System.get("../../lib/extendActor" + '');
