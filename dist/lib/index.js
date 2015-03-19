9545b039afc29baef7f88683644b20e6c4057101
leogiese <leogiese0@gmail.com>
2015-03-19 09:32:50 +0800

    delete api doc from master branches

---
+++ dist/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+exports.Domain = require("./Domain");
+exports.Actor = require("./Actor");
+exports.AbstractActor = require("./AbstractActor");
+exports.DomainEvent = require("./DomainEvent");
+exports.EventBus = require("./EventBus");
+exports.Repository = require("./Repository");
