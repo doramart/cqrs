f0bf5a4b9a458a7a7078bd97f4ccf86f45de96de
leogiese <leogiese0@gmail.com>
2015-03-21 09:12:17 +0800

    clean diet

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,6 @@ var gutil = require("gulp-util");
 var concat = require("gulp-concat");
 var babel = require('gulp-babel');
 
-gulp.task("docs", function () {
-    return gulp.src("lib/*.js")
-        .pipe(concat("all.md"))
-        .pipe(jsdoc2md())
-        .on("error", function(err){
-            gutil.log("jsdoc2md failed:", err.message);
-        })
-        .pipe(gulp.dest("api"));
-});
-
 gulp.task('es5', function () {
     gulp.src('src/**/*.js')
         .pipe(babel())
